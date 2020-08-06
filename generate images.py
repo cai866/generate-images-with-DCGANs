55611d7a3f5ed7f411aa9985a1051e90e80bd678
JesseCai12 <caichao826010@gmail.com>
2020-08-06 02:49:56 -0500

    update

--- generate images.py
+++ generate images.py
@@ -1,4 +1,4 @@
-#%matplotlib inline
+
 import  tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -135,3 +135,14 @@ with imageio.get_writer(anim_file, mode='I') as writer:
 
 import IPython
 display.Image(filename=anim_file)
+
+def show(images, n_cols=None):
+    n_cols = n_cols or len(images)
+    n_rows = (len(images) - 1) // n_cols + 1
+    if images.shape[-1] == 1:
+        images = np.squeeze(images, axis=-1)
+    plt.figure(figsize=(n_cols, n_rows))
+    for index, image in enumerate(images):
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(image, cmap="binary")
+        plt.axis("off")
